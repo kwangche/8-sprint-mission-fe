d018a90fd9d09842c0d259ee0c480c98f1404ac7
kwangche <163306559+kwangche@users.noreply.github.com>
2026-01-25 18:38:08 +0900

    Revert "Next 이광체 sprint11"

--- src/app/(article)/article/edit/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ArticleEditPage from '@/components/pages/ArticleEditPage';
-
-export default function ArticleEdit() {
-  return <ArticleEditPage />;
-}
--- src/app/(items)/items/page.tsx
+++
@@ -1,6 +0,0 @@
-import ProductPage from '@/components/pages/ProductPage';
-import React from 'react';
-
-export default function Items() {
-  return <ProductPage />;
-}
